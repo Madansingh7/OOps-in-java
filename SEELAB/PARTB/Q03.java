dd898ac67306c39fa975ffbd98dc01a34f7d2f11
Madan Singh <madansingh7@yahoo.com>
2026-05-27 02:15:33 +0530

    Update Q03.java

--- SEELAB/PARTB/Q03.java
+++ SEELAB/PARTB/Q03.java
@@ -18,7 +18,7 @@ class PrimeThread extends Thread {
                 prime = false;
             }
 
-            for (int j = 2; j < i; j++) {
+            for (int j = 2; j <= i/2; j++) {
 
                 if (i % j == 0) {
                     prime = false;
